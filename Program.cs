ef6ee6089a3c7108a5e95e52405688808786dce9
Fatema Sultana <fatema.chowdury@hiottu.fi>
2021-09-15 11:38:04 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using WeatherUtilities = CS_pro.WeatherUtilities;
+
+namespace CS_pro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Where we are going");
+            WeatherUtilities.Report("Oulu", WeatherUtilities.FahrenheitToCelsius(65), 73);
+            WeatherUtilities.Report("Denver", WeatherUtilities.FahrenheitToCelsius(77), 55);
+            WeatherUtilities.Report("Bologna", 23, 65);
+        }
+
+
+
+    }
+}
---
+++ WeatherUtilities.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_pro
+{
+    class WeatherUtilities
+    {
+        static public float FahrenheitToCelsius(float temperatureFahrenheit)
+        {
+            return (temperatureFahrenheit - 32) / 1.8f;
+        }
+
+        static public float CelsiusToFahrenheit(float temperatureCelsius)
+        {
+            return temperatureCelsius * 1.8f + 32;
+        }
+        static public float ComfortIndex(float temperaturefahrenheit, float humidityPercent)
+        {
+            return (temperaturefahrenheit + humidityPercent) / 4;
+        }
+        static public void Report(string location, float temperatureCelsius, float humidity)
+        {
+            float v = CelsiusToFahrenheit(temperatureCelsius);
+            var temperatureFahrenheit = v;
+            Console.WriteLine($"Comfort Index for{location}:{ComfortIndex(temperatureFahrenheit, humidity)}");
+        }
+    }
+
+}
